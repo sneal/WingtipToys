e4bd130bb618c1dbe1560ff6cd3c45af4995d977
Shawn Neal <sneal@sneal.net>
2017-07-13 10:20:58 -0700

    Add PCF friendly error handling

--- WingtipToys/Global.asax.cs
+++ WingtipToys/Global.asax.cs
@@ -1,11 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Web;
 using System.Web.Optimization;
 using System.Web.Routing;
-using System.Web.Security;
-using System.Web.SessionState;
 using System.Data.Entity;
 using WingtipToys.Models;
 using WingtipToys.Logic;
@@ -47,21 +43,8 @@ namespace WingtipToys
 
         void Application_Error(object sender, EventArgs e)
         {
-          // Code that runs when an unhandled error occurs.
-
-          // Get last error from the server
-          Exception exc = Server.GetLastError();
-          Console.WriteLine("Unhandled exception: " + exc.Message + exc.StackTrace);
-
-          if (exc is HttpUnhandledException)
-          {
-            if (exc.InnerException != null)
-            {
-              exc = new Exception(exc.InnerException.Message);
-              Server.Transfer("ErrorPage.aspx?handler=Application_Error%20-%20Global.asax",
-                  true);
-            }
-          }
+            ExceptionUtility.LogException(Server.GetLastError());
+            Server.Transfer("ErrorPage.aspx?handler=Application_Error%20-%20Global.asax", true);
         }
     }
 }
